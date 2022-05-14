551b43473d0d579bcb9a86e82d8eca87e7bac897
Sajid Jaber <sajidjaber@yahoo.com>
2022-05-14 13:28:37 -0500

    error in add role

--- utils/prompt.js
+++ utils/prompt.js
@@ -13,6 +13,21 @@ class Prompt {
       console.table(rows);
     });
   }
+
+  getDepartments() {
+    let departmentArray = [];
+    const sql = `SELECT * FROM department`;
+    const departments = db.query(sql, (err, rows) => {
+      if (err) {
+        throw err;
+      }
+
+      rows.forEach((row) => departmentArray.push(row.title));
+
+      return departmentArray;
+    });
+  }
+
   displayDepartment() {
     const sql = `SELECT * FROM department`;
     db.query(sql, (err, rows) => {
@@ -35,22 +50,61 @@ class Prompt {
     });
   }
 
-
-  addADepartment(){
-    inquirer.prompt([
-     { type: "input",
-      name: "name",
-      message: "What is the name of department?"
-    }
-    ]).then(response => {
-      const department = db.query(`INSERT INTO department (name) VALUES (?)` , response.name , (err , data) =>{
-        if(err) throw err;
-        console.log(`${response.name} Department Created`);
-        this.startPrompt();
-      })
-    })
+  addADepartment() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the name of department?",
+        },
+      ])
+      .then((response) => {
+        db.query(
+          `INSERT INTO department (name) VALUES (?)`,
+          response.name,
+          (err, data) => {
+            if (err) throw err;
+            console.log(`${response.name} Department Created`);
+            this.startPrompt();
+          }
+        );
+      });
   }
 
+  addARole() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "role",
+          message: "What is the Role?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary?",
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "What is the department?",
+          choices: this.getDepartments(),
+        },
+      ])
+      .then((response) => {
+        const sql = `INSERT INTO role (title,salary, department_id) VALUES (?,?,?)`;
+        const indexDepartment = this.getDepartments().indexOf(response.title);
+        const params = [response.role, response.salary, indexDepartment];
+
+        db.query(sql, params, (err, data) => {
+          if (err) throw err;
+
+          console.log(`${response.role} Created`);
+          this.startPrompt();
+        });
+      });
+  }
 
   startPrompt() {
     inquirer
@@ -60,20 +114,21 @@ class Prompt {
           name: "choice",
           message: "What would you like to do?",
           choices: [
-            "View All Employees?",
-            "View All Employee's By Roles?",
+            "View All Employees",
+            "View All Employee's By Roles",
             "View all employee's By Departments",
             "Add A Department",
+            "Add A Role",
           ],
         },
       ])
       .then((response) => {
         switch (response.choice) {
-          case "View All Employees?":
+          case "View All Employees":
             this.displayEmployees();
             this.startPrompt();
             break;
-          case "View All Employee's By Roles?":
+          case "View All Employee's By Roles":
             this.displayRole();
             this.startPrompt();
             break;
@@ -83,6 +138,9 @@ class Prompt {
             break;
           case "Add A Department":
             this.addADepartment();
+            break;
+          case "Add A Role":
+            this.addARole();
             break;
         }
       });
