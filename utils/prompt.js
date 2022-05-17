27e106a60ede4def121d8ccb2ab645d965892f68
Sajid Jaber <sajidjaber@yahoo.com>
2022-05-16 19:15:59 -0500

    setup the add employee

--- utils/prompt.js
+++ utils/prompt.js
@@ -4,16 +4,11 @@ const cTable = require("console.table");
 const db = require("../db/connection");
 class Prompt {
   displayEmployees() {
-
-
-
-
-    const sql = `SELECT employee.first_name, employee.last_name, role.title, department.name, role.salary, employee2.first_name AS Manager FROM employee, role, department, employee AS employee2  WHERE department.id=role.department_id AND role.id = employee.role_id AND employee.manager_id= employee2.id`;
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee2.first_name AS Manager FROM employee, role, department, employee AS employee2  WHERE department.id=role.department_id AND role.id = employee.role_id AND employee.manager_id= employee2.id`;
     db.query(sql, (err, rows) => {
       if (err) {
         throw err;
       }
-      
 
       console.table(rows);
     });
@@ -54,6 +49,17 @@ class Prompt {
     });
   }
 
+  getRole() {
+    let roleArray = [];
+    const sql = `SELECT title FROM role`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      rows.forEach((row) => roleArray.push(row.title));
+    });
+    return roleArray;
+  }
   addADepartment() {
     inquirer
       .prompt([
@@ -127,7 +133,84 @@ class Prompt {
         });
       });
   }
+  getManager() {
+    let managerArray = [];
+    const sql = `SELECT first_name FROM employee`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      rows.forEach((row) => managerArray.push(row.first_name));
+    });
+    return managerArray;
+  }
 
+  addAEmployee() {
+    const roles = this.getRole();
+    const managers = this.getManager();
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the employee first name?",
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the employee last name?",
+        },
+        {
+          type: "list",
+          name: "role",
+          message: "What is the employee role?",
+          choices: this.getRole(),
+        },
+        {
+          type: "list",
+          name: "manager",
+          message: "What is the employee role?",
+          choices: this.getManager(),
+        },
+      ])
+      .then((response) => {
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)`;
+
+        let indexRole;
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i] == response.role) {
+            indexRole = i + 1;
+            break;
+          }
+        }
+
+        let indexManager;
+        for (let i = 0; i < managers.length; i++) {
+          if (managers[i] == response.manager) {
+            indexManager = i + 1;
+            break;
+          }
+        }
+
+        const params = [
+          response.first_name,
+          response.last_name,
+          indexRole,
+          indexManager,
+        ];
+
+        db.query(sql, params, (err, rows) => {
+          if (err) {
+            throw err;
+          }
+
+          console.log(`${response.first_name} Created`);
+      this.startPrompt();
+
+        });
+        
+      });
+    }
   startPrompt() {
     inquirer
       .prompt([
@@ -141,6 +224,7 @@ class Prompt {
             "View all employee's By Departments",
             "Add A Department",
             "Add A Role",
+            "Add A Employee",
           ],
         },
       ])
@@ -163,6 +247,9 @@ class Prompt {
             break;
           case "Add A Role":
             this.addARole();
+            break;
+          case "Add A Employee":
+            this.addAEmployee();
             break;
         }
       });
